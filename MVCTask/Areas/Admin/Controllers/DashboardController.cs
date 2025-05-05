0d0fb4b22eec775fc719b50c97197da7bd8abb06
Nezrin <nazrinna-bb208@code.edu.az>
2025-05-05 10:56:18 +0400

    first commit

---
+++ MVCTask/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCTask.Context;
+
+namespace MVCTask.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public DashboardController(AppDbContext appDbContext )
+        {
+            _db = appDbContext;
+        }
+        public IActionResult Index()
+        {
+            var categories = _db.Categories.toList();
+            return View();
+        }
+    }
+}
---
+++ MVCTask/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCTask.Models;
+
+namespace MVCTask.Context
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<HealthCareSolution> HealthCareSolutions { get; set; }
+        public object Categories { get; internal set; }
+
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+    }
+}
